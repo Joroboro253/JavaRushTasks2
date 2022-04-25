3d53efe7026dcd1ed630ac6e5f1312ef0be925c1
Joroboro253 <vlyashchenko01@gmail.com>
2022-04-25 12:30:29 +0300

    Fine

--- 1.JavaSyntax/src/com/javarush/task/pro/task14/task1415/MyStack.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task14/task1415/MyStack.java
@@ -12,22 +12,33 @@ public class MyStack {
     private final List<String> storage = new ArrayList<>();
 
     public void push(String s) {
-        //напишите тут ваш код
+        storage.add(0, s);
     }
 
     public String pop() {
-        //напишите тут ваш код
+        String str = storage.get(0);
+
+        storage.remove(0);
+
+        return str;
+
     }
 
     public String peek() {
-        //напишите тут ваш код
+        return storage.get(0);
     }
 
     public boolean empty() {
-        //напишите тут ваш код
+        return storage.isEmpty();
     }
 
     public int search(String s) {
-        //напишите тут ваш код
+        int index = -1;
+        for (int i = 0; i < storage.size(); i++) {
+            if(storage.get(i).equals(s)){
+                index = storage.indexOf((storage.get(i)));
+            }
+        }
+        return index;
     }
 }
